8cf94f4a47f9e69751d497070fabb7d5b763df19
kareemadesola <kareemadesola1999@gmail.com>
2022-07-25 12:04:03 +0100

    clear one or all items using useState with an array

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react'
-import Setup from "./tutorial/1-useState/setup/2-useState-basics";
+import Setup from "./tutorial/1-useState/setup/3-useState-array";
 
 function App() {
-    return (
-        <div className='container'>
-            <Setup/>
-        </div>
-    )
+    return <div className='container'>
+        <Setup/>
+    </div>;
 }
 
 export default App
--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -1,8 +1,27 @@
 import React from 'react';
-import { data } from '../../../data';
+import {data} from '../../../data';
 
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+    const [people, setPeople] = React.useState(data)
+
+    const removeItem = id => {
+        setPeople(people.filter(person => person.id !== id))
+    };
+
+    return <>
+        <h2>useState array example</h2>
+        {
+            people.map(person =>
+                <div key={person.id} className='item'>
+                    <h4>{person.name}</h4>
+                    <button onClick={() => removeItem(person.id)}>remove</button>
+                </div>)
+        }
+        <button className='btn' onClick={() => setPeople([])}>
+            clear items
+        </button>
+
+    </>
 };
 
 export default UseStateArray;
