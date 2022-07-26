1824bfb4fadad278329e023338bef02434bbe767
kareemadesola <kareemadesola1999@gmail.com>
2022-07-26 10:41:58 +0100

    use object as data in useState and change property values

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,20 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+    const [person, setPerson] = useState({name: 'person', age: 24, message: 'random message'})
+    console.log(person)
+
+    function changeMessage() {
+        setPerson({...person, message: 'hello world'})
+    }
+
+    return <>
+        <h2>useState object example</h2>
+        <h3>{person.name}</h3>
+        <h3>{person.age}</h3>
+        <h3>{person.message}</h3>
+        <button className='btn' onClick={changeMessage}>change message</button>
+    </>
 };
 
 export default UseStateObject;
