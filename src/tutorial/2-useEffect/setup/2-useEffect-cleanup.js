1bcd641e60e103bb798346ec4db8efc84579482d
kareemadesola <kareemadesola1999@gmail.com>
2022-08-04 14:05:48 +0100

    use effect with a cleanup function for event listeners

--- src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
+++ src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
@@ -4,7 +4,25 @@ import React, { useState, useEffect } from 'react';
 // second argument
 
 const UseEffectCleanup = () => {
-  return <h2>useEffect cleanup</h2>;
+  const [size, setSize] = useState(window.innerWidth)
+
+  const checkSize = () => {
+    setSize(window.innerWidth)
+  }
+  useEffect(()=>{
+    console.log('hello world')
+    window.addEventListener('resize',checkSize)
+    // return () => {
+    //   console.log('cleanup')
+    //   window.removeEventListener('resize', checkSize)
+    // }
+  },[])
+  console.log('render')
+  return <>
+    <h2>useEffect cleanup</h2>
+    <h1>Window</h1>
+    <h2>{size}px</h2>
+  </>
 };
 
 export default UseEffectCleanup;
