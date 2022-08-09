d60140c1bdd5f058dbb757f5a54a3034cb9bbd62
kareemadesola <kareemadesola1999@gmail.com>
2022-08-09 17:26:52 +0100

    short-circuiting and ternary operator
    Expressions are allowed in JSX; not statements

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
-import Setup from "./tutorial/3-conditional-rendering/setup/1-multiple-returns";
+import Setup from "./tutorial/3-conditional-rendering/setup/2-short-circuit";
 
 function App() {
   return (
     <div className="container">
-      <h1>conditional rendering</h1>
+      <h1>Short Circuit</h1>
       <Setup />
     </div>
   );
