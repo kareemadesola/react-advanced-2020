5cdda6bf0919774c4a40cdbfe6bef5331e3cb158
kareemadesola <kareemadesola1999@gmail.com>
2022-08-09 13:07:02 +0100

    multiple return and handling 404 errors

--- src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -1,7 +1,51 @@
-import React, { useState, useEffect } from 'react';
-const url = 'https://api.github.com/users/QuincyLarson';
+import React, { useState, useEffect } from "react";
+
+const url = "https://api.github.com/users/QuincyLarson";
 const MultipleReturns = () => {
-  return <h2>multiple returns</h2>;
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+  const [user, setUser] = useState("default user");
+
+  useEffect(() => {
+    fetch(url)
+      .then((resp) => {
+        if (resp.ok) {
+          return resp.json();
+        } else {
+          setIsLoading(false);
+          setIsError(true);
+          throw new Error(resp.statusText);
+        }
+      })
+      .then((user) => {
+        setUser(user.login);
+        setIsLoading(false);
+        // console.log(user);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div>
+        <h2>Loading...</h2>
+      </div>
+    );
+  }
+
+  if (isError) {
+    return (
+      <div>
+        <h2>Error...</h2>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <h2>{user}</h2>
+    </div>
+  );
 };
 
 export default MultipleReturns;
