fbac29124179a94265491b6f8949710fc66108d4
kareemadesola <kareemadesola1999@gmail.com>
2022-08-11 07:29:06 +0100

    show, hide, clean up with useEffect return

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
-import Setup from "./tutorial/3-conditional-rendering/setup/2-short-circuit";
+import Setup from "./tutorial/3-conditional-rendering/setup/3-show-hide";
 
 function App() {
   return (
     <div className="container">
-      <h1>Short Circuit</h1>
+      <h1>Show/Hide</h1>
       <Setup />
     </div>
   );
