24c8be9119cfd3869b16802384c8887ce66b33e3
kareemadesola <kareemadesola1999@gmail.com>
2022-08-31 12:04:46 +0100

    propTypes and defaultProps

---
+++ src/tutorial/10-prop-types/setup/testing.js
@@ -0,0 +1,9 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Testing = (props) => <div></div>;
+Testing.propTypes = {
+  name: PropTypes.array.isRequired,
+};
+
+export default Testing;
